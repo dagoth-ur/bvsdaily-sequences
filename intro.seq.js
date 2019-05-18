71f008e5d246f62de21ea230a748c25b4d57caa4
Chrobot <chrobot.bvs@gmail.com>
2019-05-18 22:50:33 +0200

    Working on daily intro sequence.

--- intro.seq.js
+++ intro.seq.js
@@ -2,15 +2,88 @@
 
 //@SequenceCode
 
-var _testing = true;
+var dseq = (function () {
+    var dseq = {};
 
-//------------------------------------------
+    function dbg(msg) {
+        console.log(msg);
+    }
+    dseq.dbg = dbg;
+
+    function DocRegex(re, idx) { "use strict";
+        dbg('Regexing: ' + re);
+        var result = re.exec(document.body.innerText);
+        dbg('Regex result: ' + result);
+        if (idx == undefined || typeof idx != 'number' || result == null) {
+            return result;
+        } else {
+            return result[idx];
+        }
+    }
+    dseq.DocRegex = DocRegex;
+
+    function DocReInt(re) { "use strict";
+        return parseInt(DocRegex(re, 1));
+    }
+    dseq.DocReInt = DocReInt;
+
+    function KaijuAttack() {
+        var times = DocReInt(/times fought today: (\d+)/);
+        IncrementTaskIf(times >= 6);
+        if (times < 4) {
+            FormCheck('kat', 'tsukiball');
+        } else {
+            FormUncheck('kat', 'tsukiball');
+        }
+        if (!DocTest('Crippled!')) {
+            var jutsu = 'Bring Down the House Jutsu';
+            var jutsuCost = DocReInt(new RegExp(jutsu + ' \\((\\d+) C\\)'));
+            dbg('Cost: ' + jutsuCost);
+            var chakra = DocReInt(/Chakra: (\d+)/);
+            if (jutsuCost > chakra)
+                FormSubmit('chakra');
+            FormCheck('kat', 'jutsuused', jutsu);
+        } else {
+            FormCheck('kat', 'jutsuused', 'none');
+        }
+        FormSubmit('kat');
+    }
+    dseq.KaijuAttack = KaijuAttack;
+
+    function TeamSolo() {
+        GoPage('team');
+        if (FormTest('conteam'))
+            FormSubmit('conteam');
+        IncrementTaskIf(DocTest('<b>Solo</b> - no Teammates, no Bonus'));
+        FormSubmit('maketeam');
+    }
+    dseq.TeamSolo = TeamSolo;
+
+    function VillageAction() {
+        if (DocTest('You are already helping out your Village today')
+                || DocTest('You are helping a Village out today'))
+            return;
+        var upkeep = DocReInt(/Current Upkeep: (\d+)%/);
+        var paperwork = DocReInt(/Paperwork:\D*(\d+)% Upkeep Tomorrow/);
+        dbg('Upkeep detected: ' + upkeep);
+        dbg('Paperwork detected: ' + paperwork);
+        if (paperwork < upkeep) {
+            FormSubmit('paperwork');
+        } else {
+            ShowMsg('Finish VillageAction()');
+        }
+    }
+    dseq.VillageAction = VillageAction;
+
+    return dseq;
+})();
+
 //@NewTask
 //@TaskName: Breakfast bingo
 
 GoPage('breakfast');
 
-IncrementTaskIf(false); // TODO
+IncrementTaskIf(DocTest('Breakfast Results:'));
 
 FormCheck('dobfast', 'bfconfirm');
 FormCheck('dobfast', 'tr-bfast');
@@ -23,7 +96,7 @@ FormCheck('dobfast', 'bingo-b5');
 
 FormSubmit('dobfast');
 
-//------------------------------------------
+
 //@NewTask
 //@TaskName: Pachinko
 
@@ -50,19 +123,19 @@ if (DocTest('You survey the rows of machines, including the ominous one'
 if (DocTest('Buys today: 0'))
     FormSubmit('buyballs');
 // Goto first machine
-if (FormRadioById('playapachi', 'pmach1'))
+if (FormCheckById('playapachi', 'pmach1'))
     FormSubmit('playapachi');
 
-//------------------------------------------
 //@NewTask
 //@TaskName: Tsukiball
+
 GoPage('ph_tsukiball');
 
-if (FormRadio('skic', 'tixtype', 'plastic'))
+if (FormCheck('skic', 'tixtype', 'plastic'))
     FormSubmit('skic');
 
 if (DocTest('You are on Ball')) {
-    FormRadio('skib', 'rolltype', 'high');
+    FormCheck('skib', 'rolltype', 'high');
     FormCheck('skib', 'doemall');
     FormSubmit('skib');
 } else if (!DocTest('Final Score:')) {
@@ -72,15 +145,70 @@ if (DocTest('You are on Ball')) {
 
 IncrementTask();
 
-//
 //@NewTask
 //@TaskName: Strawberry Team
 
-GoPage('team');
 TeamChange('Strawberry', 'Shorty', 'Robogirl');
-IncrementTask();
 
-//
 //@NewTask
 //@TaskName: Arena stuff
+
 GoPage('arena');
+
+if (DocTest('buy-ins so far today: 0')
+        || DocTest('buy-ins so far today: 1'))
+    FormSubmit('buyfights');
+
+IncrementTaskIf(!DocTest('Fights today: <b>0</b>'));
+
+FormCheck('arenafight', 'megaarena');
+FormSubmit('arenafight');
+
+//@NewTask
+//@TaskName: Fight kaiju
+
+GoPage('kaiju');
+
+if (!DocTest("You had this Kaiju's drop"))
+    ShowMsg("ZOMG, drop you don't have yet. Do this manually");
+
+dseq.KaijuAttack();
+
+//@NewTask
+//@TaskName: Switching to solo
+
+dseq.TeamSolo();
+
+//@NewTask
+//@TaskName: Village actions
+
+GoPage('village');
+
+dseq.VillageAction();
+if (!DocTest('Already Searched Today!')) {
+    ShowMsg('Fix ingredient search to use positive condition');
+    FormCheck('ingredienthunt', 'ingredientplace', 'forest');
+    FormSubmit('ingredienthunt');
+}
+if (!DocTest('already had some Lemonade today')) {
+    ShowMsg('Fix lemonade thing!');
+    FormSubmit('lemonaid');
+}
+if (DocTest('Go to a Black Stones Concert!'))
+    FormSubmit('blackstones');
+if (DocTest('Study at the Pandora Library!'))
+    FormSubmit('pandtime');
+if (FormCheck('ramen', 'ramentobuy', 'app'))
+    FormSubmit('ramen');
+
+IncrementTask();
+
+//@NewTask
+//@TaskName: Field actions
+
+ShowMsg('Do them manually for now, because that requires statefulness');
+
+//@NewTask
+//@TaskName: Nomming
+
+ShowMsg('TODO: finish this');
